3eb9d2e7a21c3ec998ab89bac5d7ee5911ea80a2
Daniel Boba <62248969+DanielSBoba@users.noreply.github.com>
2023-11-05 09:48:20 -0500

    chore: fiixing history

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,75 +1,85 @@
 <template>
-  <div class="flex justify-center">
+  <div class="flex justify-center w-screen h-screen overflow-y-scroll">
     <div
       class="absolute top-0 h-[300px] w-full bg-gradient-to-b from-green-100 to-white -z-10"
     />
     <div class="max-w-md w-full">
       <TheHeader class="pb-4" />
 
+      <TheDropzone />
       <Combobox
         :label="'Boring List'"
         :items="candidateLabelSets"
         @selected="(e) => selectList(e.name)"
       />
-      <!-- <Combobox
-        :label="'Parameter'"
-        :items="['P&W Benchmark']"
-        @selected="selectList"
-      /> -->
-      <link rel="manifest" href="manifest.json" />
-      <div class="flex flex-col">
+
+      <Transition
+        enter-active-class="transition ease-out duration-1000"
+        enter-from-class="transform opacity-0 scale-95"
+        enter-to-class="transform opacity-100 scale-100"
+      >
         <div
-          v-for="(items, key) of labelGroups"
-          class="border-b-2 border-slate-200 w-full py-4 px-4"
+          v-if="isLoading"
+          class="w-full h-full flex items-center justify-center"
         >
+          <TheLoader />
+        </div>
+        <div class="flex flex-col" v-else>
           <div
-            class="flex w-full justify-between"
-            @click.stop="expand(key.toString())"
+            v-for="(items, key) of labelGroups"
+            class="border-b border-gray-200 w-full py-4 px-4"
           >
-            <div class="flex items-center">
-              <ChevronDownIcon class="h-5" />
-
-              {{ items.length }}
-            </div>
-            <div class="bg-gray-200 text-gray-800 rounded-full px-2">
-              <!-- {{ label.labels[0] }} -->
-              {{ key }}
-            </div>
-            <!-- <div>{{ [...label.labels].splice(0, 3) }}</div> -->
-          </div>
-          <Collapse :when="expanded === key.toString()" class="v-collapse">
             <div
-              v-for="item in items"
-              class="flex justify-between w-full space-y-2 items-center"
+              class="flex w-full justify-between"
+              @click.stop="expand(key.toString())"
             >
-              <p class="flex-none truncate w-[55%]">
-                {{ item.sequence }}
-              </p>
-              <div class="flex space-x-2 w-full justify-between">
-                <p class="text-slate-300 text-sm">
-                  {{ Math.round(item.scores[0] * 100) }}%
-                </p>
-                <Dropdown :items="mapLabels(item)" class="w-32 flex-none" />
-              </div>
+              <div class="flex items-center">
+                <ChevronDownIcon class="h-5" />
 
-              <!-- add a dropdown here -->
+                {{ items.length }}
+              </div>
+              <div class="bg-gray-200 text-gray-800 rounded-full px-2">
+                <!-- {{ label.labels[0] }} -->
+                {{ key }}
+              </div>
+              <!-- <div>{{ [...label.labels].splice(0, 3) }}</div> -->
             </div>
-          </Collapse>
+            <Collapse :when="expanded === key.toString()" class="v-collapse">
+              <div
+                v-for="item in items"
+                class="flex justify-between w-full space-y-2 items-center"
+              >
+                <p class="flex-none truncate w-[55%]">
+                  {{ item.sequence }}
+                </p>
+                <div class="flex space-x-2 w-full justify-between">
+                  <p class="text-slate-300 text-sm">
+                    {{ Math.round(item.scores[0] * 100) }}%
+                  </p>
+                  <Dropdown :items="mapLabels(item)" class="w-32 flex-none" />
+                </div>
+
+                <!-- add a dropdown here -->
+              </div>
+            </Collapse>
+          </div>
+          <div class="w-full justify-end flex p-4">
+            <button
+              @click="handleSave"
+              class="bg-green-950 text-gray-100 font-semibold rounded-lg px-4 py-2"
+            >
+              Save
+            </button>
+          </div>
         </div>
-        <div class="w-full justify-end flex p-4">
-          <button
-            @click="handleSave"
-            class="bg-green-950 text-gray-100 font-semibold rounded-lg px-4 py-2"
-          >
-            Save
-          </button>
-        </div>
-      </div>
+      </Transition>
     </div>
   </div>
 </template>
 <script lang="ts" setup>
 import TheHeader from "./components/TheHeader.vue";
+import TheLoader from "./components/TheLoader.vue";
+import TheDropzone from "./components/TheDropzone.vue";
 import { computed, onMounted, ref, watch } from "vue";
 import { pwBenchmark } from "./constats/pw-benchmark";
 import { rooms } from "./constats/rooms";
@@ -94,11 +104,15 @@ const labels = computed(() => store.labels);
 
 const labelGroups = ref("");
 
+const isLoading = ref(false);
+
 const expanded = ref("");
 const list = ref("placeholfer");
 
 async function getLabels(item: any) {
   console.log(candidateLabels.value);
+
+  isLoading.value = true;
 
   store.labels = [];
 
@@ -136,6 +150,7 @@ async function getLabels(item: any) {
   }
 
   await Promise.all(promises);
+  isLoading.value = false;
 
   if (!labels.value.length) return;
 
---
+++ app/src/components/TheDropzone.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="w-full border-white border-4 bg-opacity-30 bg-white hover:border-green-200 transition rounded-full flex items-center justify-center h-10"
+  >
+    <p>Drop it</p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
