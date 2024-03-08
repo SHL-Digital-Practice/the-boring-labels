e2d7d4ec8ec565ee14d17912400fa62dd8be9aaf
Victor Wanderley Barbosa <vwb@shl.dk>
2024-03-08 14:24:25 +0100

    fix: padding

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -5,14 +5,14 @@ import ParameterSelect from "./components/parameter-select";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex min-h-screen flex-col items-center p-8">
       <div className="max-w-5xl w-full items-center justify-center">
         <form
           action={async (formData: FormData) => {
             "use server";
             console.log("formData", formData);
           }}
-          className="space-y-6"
+          className="flex flex-col gap-y-6"
         >
           <CategorySelect />
           <ParameterSelect />
