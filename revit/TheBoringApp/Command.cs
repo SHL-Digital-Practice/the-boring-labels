2a6cf92c7842f95955c0f180d1b6849be28196fe
Victor Wanderley Barbosa <vwb@shl.dk>
2024-03-08 13:42:07 +0100

    add wpf

--- revit/TheBoringApp/Command.cs
+++ revit/TheBoringApp/Command.cs
@@ -2,18 +2,24 @@ using System.Reflection;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using TheBoringApp.views;
 
 namespace TheBoringApp
 {
-  [Transaction(TransactionMode.Manual)]
-  public class Command : IExternalCommand
-  {
-    public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
     {
-      string version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            string version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
 
-      TaskDialog.Show($"v{version}", "Hello, Revit!");
-      return Result.Succeeded;
+            TaskDialog.Show($"v{version}", "Hello, Revit!");
+
+            var window = new BoringAppWindow();
+            window.Activate();
+            window.Show();
+
+            return Result.Succeeded;
+        }
     }
-  }
 }
---
+++ revit/TheBoringApp/views/BoringAppWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TheBoringApp.views
+{
+    /// <summary>
+    /// Interaction logic for BoringAppWindow.xaml
+    /// </summary>
+    public partial class BoringAppWindow : Window
+    {
+        public BoringAppWindow()
+        {
+            InitializeComponent();
+            string version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            this.Title = $"Boring App v{version}";
+        }
+    }
+}
