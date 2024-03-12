b65922fba856deb4d73832db24348e4b3dc6e5e4
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2024-03-12 20:26:22 +0100

    fun nav

---
+++ app/app/components/nav.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { FrownIcon, LaughIcon, MenuIcon } from "lucide-react";
+import { useHover } from "@uidotdev/usehooks";
+
+export default function Nav() {
+  const [ref, hovering] = useHover();
+
+  console.log("hovering", hovering);
+  return (
+    <nav className="px-6 py-4 flex justify-between bg-primary rounded-2xl mt-2 mx-2">
+      <MenuIcon className="text-primary-foreground" />
+      <ul ref={ref}>
+        {hovering ? (
+          <FrownIcon className="text-primary-foreground" />
+        ) : (
+          <LaughIcon className="text-primary-foreground" />
+        )}
+      </ul>
+    </nav>
+  );
+}
