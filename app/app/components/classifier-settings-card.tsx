6ac44bc650215f4cb6054a745fc7edfbcc5c372f
Victor Wanderley Barbosa <vwb@shl.dk>
2024-04-04 14:06:34 +0200

    collapse settings

--- app/app/components/classifier-settings-card.tsx
+++ app/app/components/classifier-settings-card.tsx
@@ -8,6 +8,12 @@ import {
 import DictionarySelect from "./dictionary-select";
 import ParameterSelect from "./parameter-select";
 import { useSearchParams } from "next/navigation";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { AccordionContent } from "@radix-ui/react-accordion";
 
 export default function ClassifierSettingsCard({
   parameters,
@@ -18,19 +24,27 @@ export default function ClassifierSettingsCard({
   const category = searchParams.get("category") || "";
 
   return (
-    <Card className="md:w-2/5">
-      <CardHeader>
-        <CardTitle>Settings</CardTitle>
-        <CardDescription>
-          Choose how you want to classify you data.
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="flex flex-col gap-y-6 ">
-          <ParameterSelect category={category} parameters={parameters} />
-          <DictionarySelect />
-        </div>
-      </CardContent>
-    </Card>
+    <Accordion type="single" collapsible className="w-full">
+      <AccordionItem value="settings-card">
+        <Card className="md:w-2/5">
+          <CardHeader>
+            <AccordionTrigger className="text-start">
+              <CardTitle>Settings</CardTitle>
+            </AccordionTrigger>
+            <CardDescription>
+              Choose how you want to classify you data.
+            </CardDescription>
+          </CardHeader>
+          <AccordionContent>
+            <CardContent>
+              <div className="flex flex-col gap-y-6 ">
+                <ParameterSelect category={category} parameters={parameters} />
+                <DictionarySelect />
+              </div>
+            </CardContent>
+          </AccordionContent>
+        </Card>
+      </AccordionItem>
+    </Accordion>
   );
 }
