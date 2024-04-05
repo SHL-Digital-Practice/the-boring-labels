09337636d71e7a4cf7e55fc607b4604d9dc493cf
Victor Wanderley Barbosa <vwb@shl.dk>
2024-04-05 11:54:38 +0200

    feat: added pagination

--- app/app/components/results-table.tsx
+++ app/app/components/results-table.tsx
@@ -2,6 +2,7 @@ import {
   ColumnDef,
   flexRender,
   getCoreRowModel,
+  getPaginationRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 import { Result } from "../classifier/components/result-column";
@@ -14,6 +15,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
 
 export const mockResults: Result[] = [
   {
@@ -31,6 +33,91 @@ export const mockResults: Result[] = [
     candidate: "Condo",
     labels: ["Condo", "Apartment", "House"],
   },
+  {
+    id: 4,
+    candidate: "House",
+    labels: ["House", "Condo", "Apartment"],
+  },
+  {
+    id: 5,
+    candidate: "Apartment",
+    labels: ["Apartment", "House", "Condo"],
+  },
+  {
+    id: 6,
+    candidate: "Condo",
+    labels: ["Condo", "Apartment", "House"],
+  },
+  {
+    id: 7,
+    candidate: "House",
+    labels: ["House", "Condo", "Apartment"],
+  },
+  {
+    id: 8,
+    candidate: "Apartment",
+    labels: ["Apartment", "House", "Condo"],
+  },
+  {
+    id: 9,
+    candidate: "Condo",
+    labels: ["Condo", "Apartment", "House"],
+  },
+  {
+    id: 10,
+    candidate: "House",
+    labels: ["House", "Condo", "Apartment"],
+  },
+  {
+    id: 11,
+    candidate: "Apartment",
+    labels: ["Apartment", "House", "Condo"],
+  },
+  {
+    id: 12,
+    candidate: "House",
+    labels: ["House", "Condo", "Apartment"],
+  },
+  {
+    id: 13,
+    candidate: "Condo",
+    labels: ["Condo", "Apartment", "House"],
+  },
+  {
+    id: 14,
+    candidate: "House",
+    labels: ["House", "Condo", "Apartment"],
+  },
+  {
+    id: 15,
+    candidate: "Apartment",
+    labels: ["Apartment", "House", "Condo"],
+  },
+  {
+    id: 16,
+    candidate: "Condo",
+    labels: ["Condo", "Apartment", "House"],
+  },
+  {
+    id: 17,
+    candidate: "House",
+    labels: ["House", "Condo", "Apartment"],
+  },
+  {
+    id: 18,
+    candidate: "Apartment",
+    labels: ["Apartment", "House", "Condo"],
+  },
+  {
+    id: 19,
+    candidate: "Condo",
+    labels: ["Condo", "Apartment", "House"],
+  },
+  {
+    id: 20,
+    candidate: "House",
+    labels: ["House", "Condo", "Apartment"],
+  },
 ];
 
 interface ResultsTableProps<TData, TValue> {
@@ -46,49 +133,73 @@ export default function ResultsTable<TData, TValue>({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
   });
   return (
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </TableHead>
-                );
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
+    <div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
               </TableRow>
-            ))
-          ) : (
-            <TableCell colSpan={columns.length} className="h-24 text-center">
-              No results.
-            </TableCell>
-          )}
-        </TableBody>
-      </Table>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
+        </Button>
+      </div>
     </div>
   );
 }
