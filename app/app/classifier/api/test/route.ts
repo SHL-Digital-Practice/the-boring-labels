769fee9ba2ead1a398161f70b60bb725fe72a512
Victor Wanderley Barbosa <vwb@shl.dk>
2024-04-05 16:08:11 +0200

    chore: bump build

--- app/app/classifier/api/test/route.ts
+++ app/app/classifier/api/test/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
-  return Response.json({ message: "Hello, world!" });
+  return Response.json({ message: "Hello, world 2!" });
 }
