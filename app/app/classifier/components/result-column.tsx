bd93d51e15dfc6256d22eabb0bc9c643f3995dea
Victor Wanderley Barbosa <vwb@shl.dk>
2024-04-05 11:28:39 +0200

    cell formatting

--- app/app/classifier/components/result-column.tsx
+++ app/app/classifier/components/result-column.tsx
@@ -1,4 +1,16 @@
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { ColumnDef } from "@tanstack/react-table";
+import { useState } from "react";
 
 export type Result = {
   id: number;
@@ -13,6 +25,35 @@ export const columns: ColumnDef<Result>[] = [
   },
   {
     accessorKey: "labels",
-    header: "Labels",
+    header: () => <div className="text-right">Labels</div>,
+    cell: ({ row }) => {
+      const labels = row.getValue("labels") as Result["labels"];
+
+      return <ResultLabelsCell labels={labels} />;
+    },
   },
 ];
+
+function ResultLabelsCell({ labels }: { labels: [string, string, string] }) {
+  const [selectedLabel, setSelectedLabel] = useState(labels[0]);
+
+  return (
+    <div className="flex justify-end ">
+      <Select value={selectedLabel} onValueChange={setSelectedLabel}>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Select a fruit" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Results</SelectLabel>
+            {labels.map((label) => (
+              <SelectItem value={label} key={label}>
+                {label}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+}
